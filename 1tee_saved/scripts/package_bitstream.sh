634310d39bba14885a4f8667c21ad6c37d1ee6f2
e <ericnie7@gmail.com>
2025-11-24 14:30:51 -0500

    cleanup 2

--- 1tee_saved/scripts/package_bitstream.sh
+++
@@ -1,9 +0,0 @@
-updatemem -force \
-	-meminfo ../platform_riscv/hw/sdt/OSDZU3x_BYOTEE_wrapper.mmi \
-	-bit ../platform_riscv/hw/sdt/OSDZU3x_BYOTEE_wrapper.bit \
-	-data ../riscv_firmware/build/riscv_firmware.elf \
-	-proc OSDZU3x_BYOTEE_i/microblaze_riscv_0 \
-	-out system_fw.bit \
-	-force
-
-rm *.log *.jou
